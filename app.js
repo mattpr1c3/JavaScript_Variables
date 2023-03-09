dc8c4a022d5d4e918211d6baed938a7ef404a892
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-09 10:13:24 -0700

    created variables for exercise

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Matthew";
+let lastName = "Price"
+var age = 28;
 // Exercise 2
-
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 // Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stack = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
